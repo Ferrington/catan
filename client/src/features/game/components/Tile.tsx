bca005957d8d9662c6f88d2a89417756de87ef94
Christopher Bandurski <cbandurski1@gmail.com>
2023-08-27 19:19:29 -0500

    improved visibility of pieces

--- client/src/features/game/components/Tile.tsx
+++ client/src/features/game/components/Tile.tsx
@@ -5,20 +5,40 @@ import rockImg from "../../../assets/resources/rock.svg";
 import wheatImg from "../../../assets/resources/wheat.svg";
 import woodImg from "../../../assets/resources/wood.svg";
 
-type Imgs = {
-  brick: string;
-  sheep: string;
-  rock: string;
-  wheat: string;
-  wood: string;
+type ResourceVisuals = {
+  brick: ResourceVisual;
+  sheep: ResourceVisual;
+  rock: ResourceVisual;
+  wheat: ResourceVisual;
+  wood: ResourceVisual;
 };
 
-const imgs: Imgs = {
-  brick: brickImg,
-  sheep: sheepImg,
-  rock: rockImg,
-  wheat: wheatImg,
-  wood: woodImg,
+type ResourceVisual = {
+  img: string;
+  color: string;
+}
+
+const resourceVisuals: ResourceVisuals = {
+  brick: {
+    img: brickImg,
+    color: "#ffb0b0",
+  },
+  sheep: {
+    img: sheepImg,
+    color: "#9bff9b",
+  },
+  rock: {
+    img: rockImg,
+    color: "#d3d3d3",
+  },
+  wheat: {
+    img: wheatImg,
+    color: "#ffe140",
+  },
+  wood: {
+    img: woodImg,
+    color: "#d78e71",
+  },
 }
 
 export default function Tile({tile}: { tile: TileType }) {
@@ -30,10 +50,12 @@ export default function Tile({tile}: { tile: TileType }) {
   return (
     <div className="tile" style={{
       top: tile.top,
-      left: tile.left
+      left: tile.left,
+      background: tile.resource == null ? "#fff4b1" : resourceVisuals[tile.resource].color,
     }}>
       <div className="text">
-        {tile.resource != null && <img src={imgs[tile.resource]} width={40} height={40} alt="resource icon"/>}
+        {tile.resource != null &&
+            <img src={resourceVisuals[tile.resource].img} width={40} height={40} alt="resource icon"/>}
         <div style={{position: "absolute", top: 20}}>{coords.toString()}</div>
       </div>
     </div>
--- client/src/features/game/components/Vertex.tsx
+++ client/src/features/game/components/Vertex.tsx
@@ -7,11 +7,14 @@ export default function Vertex({vertex}: { vertex: VertexType }) {
   let building = null;
   if (vertex.building != null) {
     if (vertex.building.type === "city")
-      building = <CityImg fill={vertex.building.color}/>;
+      building =
+        <CityImg fill={vertex.building.color}
+                 style={{boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"}}/>;
     else if (vertex.building.type === "settlement")
-      building = <SettlementImg fill={vertex.building.color}/>;
+      building =
+        <SettlementImg fill={vertex.building.color}
+                       filter="drop-shadow(0px 0px 1px black)"/>;
   }
-
 
   return (
     <div className="vertex" style={{
